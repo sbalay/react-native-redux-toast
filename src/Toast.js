a6f328a1302c757dd5c811ad40751b5f6562b43e
Abdul Fattah Ikhsan <skull.saders18@gmail.com>
2017-12-16 06:57:11 +0700

    Use logical 'or' instead of ternary operator

--- src/Toast.js
+++ src/Toast.js
@@ -6,7 +6,7 @@ import styles from './Toast.styles';
 
 import { actionCreators as toastActions } from './redux/actions';
 
-const ViewPropTypes = RNViewPropTypes ? RNViewPropTypes : View.propTypes;
+const ViewPropTypes = RNViewPropTypes || View.propTypes;
 
 export default class Toast extends Component {
   state = {
